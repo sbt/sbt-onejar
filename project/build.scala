336959f31f04652c5dd6514bd01acbd22f587c1b
Jason Zaugg <jzaugg@gmail.com>
2011-11-08 08:18:55 +0100

    remove scripted plugin, v0.6

--- project/build.scala
+++ project/build.scala
@@ -5,9 +5,9 @@ object build extends Build {
   val sbtXjc = Project(
     id = "sbt-onejar",
     base = file("."),
-    settings = Defaults.defaultSettings ++ ScriptedPlugin.scriptedSettings ++ Seq[Project.Setting[_]](
+    settings = Defaults.defaultSettings ++ Seq[Project.Setting[_]](
       organization := "com.github.retronym",
-      version := "0.6-SNAPSHOT",
+      version := "0.6",
       sbtPlugin := true,
       scalacOptions in Compile ++= Seq("-deprecation"),
       publishTo <<= (version) { (v: String) =>
