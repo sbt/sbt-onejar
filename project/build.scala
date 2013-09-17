014411d3fe147eb4941f8ccf661b7620e27bc549
Jason Zaugg <jzaugg@gmail.com>
2013-09-17 10:22:21 +0200

    bump version

--- project/build.scala
+++ project/build.scala
@@ -7,7 +7,7 @@ object build extends Build {
     base = file("."),
     settings = Defaults.defaultSettings ++ ScriptedPlugin.scriptedSettings ++ Seq[Project.Setting[_]](
       organization := "com.github.retronym",
-      version := "0.8-SNAPSHOT",
+      version := "0.9-SNAPSHOT",
       sbtPlugin := true,
       scalacOptions in Compile ++= Seq("-deprecation"),
       publishTo := Some(Resolver.url("sbt-plugin-releases", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns)),
